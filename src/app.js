80a3f4e047e446bae0afb20a14557820ea64ab09
Alina Shumann <ashumann13@gmail.com>
2022-03-04 09:44:09 +0000

    dynamic page

--- src/app.js
+++ src/app.js
@@ -3,30 +3,45 @@ import fs from 'fs/promises'
 import { basename, join } from 'path'
 
 const pkg = JSON.parse(await fs.readFile('./package.json'))
-const modfilename = `mods-${pkg.version}.zip`
 
-const clientZip = new ZIP()
-const serverZip = new ZIP()
+await generateMods()
+await generateWeb()
 
-const [common, client, server] = await Promise.all([
-  'mods/common',
-  'mods/client',
-  'mods/server'
-].map(async d => {
-  const files = await fs.readdir(d)
-  return files.map(f => join(d, f))
-}))
+async function generateWeb() {
+  const files = await fs.readdir('www')
+  await Promise.all(files.map(file => fs.copyFile(`www/${file}`, `docs/${file}`)))
+  const list = (await fs.readFile('output/mods-client.txt', 'utf8')).replace(/\n/g, '<br />\n')
+  let index = await fs.readFile('docs/index.html', 'utf8')
+  index = index.replace(/%VERSION%/g, pkg.version)
+  index = index.replace(/%MODS_LIST%/g, list)
+  index = index.replace(/%MODS%/g, `https://github.com/AlinaNova21/LunarSMP/releases/download/${pkg.version}/mods-client.zip`)
+  await fs.writeFile('docs/index.html', index)
+}
 
-const clientList = [...common, ...client]
-const serverList = [...common, ...server]
+async function generateMods() {
+  const clientZip = new ZIP()
+  const serverZip = new ZIP()
 
-clientList.forEach(f => clientZip.addLocalFile(f))
-serverList.forEach(f => serverZip.addLocalFile(f))
+  const [common, client, server] = await Promise.all([
+    'mods/common',
+    'mods/client',
+    'mods/server'
+  ].map(async d => {
+    const files = await fs.readdir(d)
+    return files.map(f => join(d, f))
+  }))
 
-await fs.mkdir('output', { recursive: true }).catch(() => { })
+  const clientList = [...common, ...client]
+  const serverList = [...common, ...server]
 
-await fs.writeFile(`output/mods-client.txt`, clientList.map(f => basename(f)).join('\n'))
-await fs.writeFile(`output/mods-server.txt`, serverList.map(f => basename(f)).join('\n'))
+  clientList.forEach(f => clientZip.addLocalFile(f))
+  serverList.forEach(f => serverZip.addLocalFile(f))
 
-clientZip.writeZip(`output/mods-client.zip`)
-serverZip.writeZip(`output/mods-server.zip`)
+  await fs.mkdir('output', { recursive: true }).catch(() => { })
+
+  await fs.writeFile(`output/mods-client.txt`, clientList.map(f => basename(f)).join('\n'))
+  await fs.writeFile(`output/mods-server.txt`, serverList.map(f => basename(f)).join('\n'))
+
+  clientZip.writeZip(`output/mods-client.zip`)
+  serverZip.writeZip(`output/mods-server.zip`)
+}
