5ce5c9d35126b25760af140bb0fe9e1b51fe7e94
Alina Shumann <ashumann13@gmail.com>
2022-03-08 02:30:06 +0000

    Merge Anemoia pack back in

--- bundle_resources.sh
+++ bundle_resources.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 cd resourcepacks/lunar_smp
-zip -r lunar_smp.zip *
-cp lunar_smp.zip ../../docs
-cp lunar_smp.zip ../../www
+cp ../Anemoia+1.17.zip ../lunar_smp.zip
+zip -r ../lunar_smp_base.zip *
+zip -r ../lunar_smp.zip *
+cd ..
+cp lunar_smp.zip ../docs
+cp lunar_smp.zip ../www
 rm lunar_smp.zip
+rm lunar_smp_base.zip
