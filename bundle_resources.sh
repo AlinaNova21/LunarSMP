46aefaa96cb1784c3270281ab2be9c1b2fe6d473
Alina Shumann <ashumann13@gmail.com>
2022-03-10 01:05:09 -0600

    Tweak site deploy

--- bundle_resources.sh
+++ bundle_resources.sh
@@ -4,7 +4,5 @@ cp ../Anemoia+1.17.zip ../lunar_smp.zip
 zip -r ../lunar_smp_base.zip *
 zip -r ../lunar_smp.zip *
 cd ..
-cp lunar_smp.zip ../docs
-cp lunar_smp.zip ../www
-rm lunar_smp.zip
-rm lunar_smp_base.zip
+mv lunar_smp.zip ../docs
+mv lunar_smp_base.zip ../docs
